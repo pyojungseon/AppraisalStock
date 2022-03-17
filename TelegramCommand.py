b136399894e5e79771e77cf210e24f42b57b400f
Pyojungseon <suehig@naver.com>
2022-03-17 09:43:16 +0900

    fix env parameter error

--- TelegramCommand.py
+++ TelegramCommand.py
@@ -7,23 +7,22 @@ tg = ''
 class TelegramCommand:
 
     def __init__(self, _env):
+        print('init telegramBot command')
+
         self._env = _env
 
         # init telegram chatbot
         self.tg = TelegramBot(_env)
 
     def start(self, update, context):
-        env_name = ''
-        if _env == 'T':
-            env_name = '[TEST]'
-        text = "[AppraisalStock] " + env_name + ". 사용법을 알고싶으시면 /help를 입력하여주세요"
-        msg = _env + "||" + str(update.effective_chat.id) + "||" + text
+        text = "[AppraisalStock] " + ". 사용법을 알고싶으시면 /help를 입력하여주세요"
+        msg = self._env + "||" + str(update.effective_chat.id) + "||" + text
         print(msg)
         tg.sendMsg(msg)
 
     def help(self, update, context):
         text = '''/get : 주가 정보
                 \n개발중.'''
-        msg = _env + "||" + str(update.effective_chat.id) + "||" + text
+        msg = self._env + "||" + str(update.effective_chat.id) + "||" + text
         print(msg)
         tg.sendMsg(msg)
