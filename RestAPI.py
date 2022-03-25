6483bb24b06282e2fb93c4cb65c83a43a2e9fa4b
Pyojungseon <suehig@naver.com>
2022-03-25 16:36:01 +0900

    fix http 415 error to remove headers

--- RestAPI.py
+++ RestAPI.py
@@ -27,19 +27,23 @@ class RestAPI:
             props = properties["PROD"]
         return props
 
-    def send_api(self, path, method):
+    def send_api(self, path, method, userData):
         API_HOST = self.props["api_host"]
         url = API_HOST + path
-        headers = {'Content-Type': 'application/json', 'charset': 'UTF-8', 'Accept': '*/*'}
-        body = {"key1": "value1", "key2": "value2"}
+        # headers = {'Content-Type': 'application/json', 'charset': 'UTF-8', 'Accept': '*/*'}
+        body = {userData[0]:userData[1], userData[2]:userData[3]}
 
         try:
             if method == 'GET':
-                response = requests.get(url, headers)
+                response = requests.get(url)
             elif method == 'POST':
-                response = requests.put(url, headers, data=json.dumps(body, ensure_ascii=False, indent="\t"))
+                response = requests.post(url, json=body)
+            elif method == 'PUT':
+                response = requests.put(url, json=body)
+
             print("response status %r" % response.status_code)
-            print("response status %r" % response.status_code)
+            print("response data %r" % response.text)
+
         except Exception as ex:
             print(ex)
 
